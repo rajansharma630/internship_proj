e1a769c605725f6ddee54efe57010d1338be8137
Rajan Sharma <226026049+rajansharma630@users.noreply.github.com>
2026-07-14 13:18:23 +0530

    Updated project

---
+++ faltu.py
@@ -0,0 +1,3 @@
+string ="At a fundamental level, NLP cleans human language (text and speech) and breaks it down into smaller parts, like sentences and individual words. These words are translated into mathematical numbers (vectors) so that algorithms can recognize patterns, syntax, and meaning. This process is what allows modern applications to read and respond naturally"
+a=string.split(',')
+print(a)
